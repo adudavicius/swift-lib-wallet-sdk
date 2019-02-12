9d87d3944ba384b3788dcd5bb17dfdaf10d865b4
Arvydas Dudavicius <arvydas.dudavicius@gmail.com>
2019-02-12 11:56:43 +0200

    PSAccountInformation fields is optional

--- Sources/Entities/Account/PSAccountInformation.swift
+++ Sources/Entities/Account/PSAccountInformation.swift
@@ -2,34 +2,33 @@ import ObjectMapper
 
 public class PSAccountInformation: Mappable {
     public let accountNumber: String
-    public let ownerDisplayName: String
-    public let ownerTitle: String
-    public let ownerType: String
-    public let type: String
-    public let userId: Int
-    public let status: String
-    public let ibans: [String]
-    public let flags: PSAccountInformationFlags
+    public var ownerDisplayName: String?
+    public var ownerTitle: String?
+    public var ownerType: String?
+    public var type: String?
+    public var userId: Int?
+    public var status: String?
+    public var ibans: [String]?
+    public var flags: PSAccountInformationFlags?
     public var accountDescription: String?
     
     required public init?(map: Map) {
         do {
             accountNumber = try map.value("number")
-            ownerDisplayName = try map.value("owner_display_name")
-            ownerTitle = try map.value("owner_title")
-            ownerType = try map.value("owner_type")
-            type = try map.value("type")
-            userId = try map.value("user_id")
-            status = try map.value("status")
-            ibans = try map.value("ibans")
-            flags = try map.value("flags")
-            
         } catch {
             return nil
         }
     }
     
     public func mapping(map: Map) {
+        ownerDisplayName    <- map["owner_display_name"]
+        ownerTitle          <- map["owner_title"]
+        ownerType           <- map["owner_type"]
+        status              <- map["status"]
+        type                <- map["type"]
+        userId              <- map["user_id"]
+        flags               <- map["flags"]
         accountDescription  <- map["description"]
+        ibans               <- map["ibans"]
     }
 }
